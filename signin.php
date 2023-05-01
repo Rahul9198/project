0a9b961786593d495217483d0604178b74e2ad07
RAHUL KUMAR VERMA <111939912+Rahul9198@users.noreply.github.com>
2023-05-01 17:14:40 +0530

    Add files via upload

---
+++ signin.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>sugn in</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+     <div class="container d-flex align-items-center justify-content-center">
+        <div class="card signin_card">
+            <!-- card header -->
+            <div class="card-header">
+                <h3 class="text-center">Sign In</h3>
+        
+            </div>
+            <!-- card body -->
+            <div class="card-body">
+                <form action="">
+                    <!-- first field -->
+                    <div class="input-group mb-3">
+  <span class="input-group-text" id="basic-addon1"><i class="fa-solid fa-user"></i></span>
+  <input type="text" class="form-control" placeholder=" Enter Your Username" required="required"autocomplete="off" aria-label="Username"name="username" aria-describedby="basic-addon1">
+</div>
+                    <!-- second field -->
+                    <div class="input-group mb-3">
+  <span class="input-group-text" id="basic-addon1"><i class="fas fa-key"></i></span>
+  <input type="password" class="form-control" placeholder=" Enter Your Password" required="required"autocomplete="off" aria-label="Password"name="Password" aria-describedby="basic-addon1">
+</div>
+                    <!-- third field -->
+                    
+                    <!-- signUp Button -->
+                    <div class="form-group">
+                        <center><input type="submit" name="signin"value="sign In" class=" btn signup_btn form-control"></center>
+                    </div>
+                    
+                </form>
+
+            </div>
+            <!-- card footer -->
+            <div class="card-footer text-center text-light signup">
+                Don't have an account?<a href="index.php">Sign Up</a>
+            </div>
+        </div>
+     </div>
+     
+   
+    
+  </body>
+</html>
